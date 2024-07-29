5ef35107372029f57fab03f98de081f08272b755
Pryce King <153151502+prycekim@users.noreply.github.com>
2024-07-29 08:46:43 +0000

    master

--- DataVisualization/app.py
+++ DataVisualization/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
-
+from flask import Flask,render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
-    return 'you have already start this project'
+    # 把csv文件里的内容送到index.html
+    data = pd.read_csv('')
+    data = data.rename(columns={'':'','':''})
+    data = data.to_dict(orient='records')
+    return render_template('index.html',data = data)
 
 
 if __name__ == 'main':
-    app.debug = True
+    app.debug = False
     app.run()
