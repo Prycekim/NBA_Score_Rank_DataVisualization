b9470f79505c71ee6e5865eca9b245a9deca5b87
Pryce King <153151502+prycekim@users.noreply.github.com>
2024-07-29 06:15:04 +0000

    master

--- DataVisualization/app.py
+++ DataVisualization/app.py
@@ -2,3 +2,12 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'you have already start this project'
+
+
+if __name__ == 'main':
+    app.debug = True
+    app.run()
